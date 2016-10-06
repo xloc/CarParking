3ab2172a9d887fd9fb023eb588beb659e3d01cde
xloc <tj-xzx@126.com>
2016-10-06 11:11:29 +0800

    Modifying code for single pic analysis

--- util/calibration.py
+++ util/calibration.py
@@ -13,25 +13,24 @@ objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('*.jpg')
+fname = 'left01.jpg'
 
-for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+img = cv2.imread(fname)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+# Find the chess board corners
+ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
 
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
+# If found, add object points, image points (after refining them)
+if ret == True:
+    objpoints.append(objp)
 
-        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        imgpoints.append(corners2)
+    corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+    imgpoints.append(corners2)
 
-        # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-        cv2.imshow('img',img)
-        cv2.waitKey(500)
+    # Draw and display the corners
+    img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
+    cv2.imshow('img',img)
+    cv2.waitKey(0)
 
 cv2.destroyAllWindows()
