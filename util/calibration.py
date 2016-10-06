ee8700744d1a8cae65b6c67901a559e7cff377e3
xloc <tj-xzx@126.com>
2016-10-06 21:30:39 +0800

    Generalize chess board size

--- util/calibration.py
+++ util/calibration.py
@@ -2,12 +2,18 @@ import numpy as np
 import cv2
 import glob
 
+boardRow = 6
+boardCol = 7
+
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+# objp = np.zeros((6*7,3), np.float32)
+# objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+
+objp = np.zeros((boardRow*boardCol,3), np.float32)
+objp[:,:2] = np.mgrid[0:boardCol,0:boardRow].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -19,7 +25,7 @@ img = cv2.imread(fname)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 # Find the chess board corners
-ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+ret, corners = cv2.findChessboardCorners(gray, (boardCol,boardRow),None)
 
 # If found, add object points, image points (after refining them)
 if ret == True:
@@ -29,8 +35,8 @@ if ret == True:
     imgpoints.append(corners2)
 
     # Draw and display the corners
-    imgr = cv2.drawChessboardCorners(img, (7,6), corners, ret)
-    img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
+    imgr = cv2.drawChessboardCorners(img, (boardCol,boardRow), corners, ret)
+    img = cv2.drawChessboardCorners(img, (boardCol,boardRow), corners2,ret)
 
     # cv2.imshow('img',img)
     # cv2.imshow('raw corners', imgr)
