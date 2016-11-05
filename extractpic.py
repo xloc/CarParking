1f0dba5a2e0c584b31c491bb518d085a542d5243
xloc <tj-xzx@126.com>
2016-11-05 17:23:36 +0800

    Add string to opencv image casting method for convenience

--- extractpic.py
+++ extractpic.py
@@ -13,3 +13,15 @@ def stream2image(stream, size):
     mtx = numpy.array(listed, dtype=numpy.uint8).reshape(shape)
 
     return mtx
+
+
+def string2image(bytes, size):
+    # type: (str, tuple) -> numpy.array
+
+    pixels = size[0]*size[1]*3
+    listed = struct.unpack('={}B'.format(pixels), bytes)
+
+    shape = (size[1], size[0], 3)
+    mtx = numpy.array(listed, dtype=numpy.uint8).reshape(shape)
+
+    return mtx
