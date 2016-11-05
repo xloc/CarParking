05313bba1500a28734ed9e543c3f36c225293b3a
xloc <tj-xzx@126.com>
2016-11-05 17:26:37 +0800

    Test the multi-frame capture logic, bug exist

--- multiframecapture.py
+++ multiframecapture.py
@@ -4,6 +4,9 @@ import threading
 import picamera
 import copy
 
+# import pydevd
+# pydevd.settrace('192.168.1.111', port=52481, stdoutToServer=True, stderrToServer=True)
+
 frameCaptured = threading.Event()
 
 streamLock = threading.Lock()
@@ -13,10 +16,11 @@ done = False
 
 
 def get_framestream():
+    # type: () -> string
     if frameCaptured.wait():
         with streamLock:
             picStream.seek(0)
-            stream = copy.copy(picStream)
+            stream = picStream.read()
 
         frameCaptured.clear()
         return stream
@@ -37,4 +41,29 @@ def capture_loop():
         camera.framerate = 30
         camera.start_preview()
         time.sleep(2)
-        camera.capture_sequence(streams_provider(), use_video_port=True)
+        print 'start capture'
+        camera.capture_sequence(streams_provider(),
+                                use_video_port=True,
+                                format='bgr')
+
+if __name__ == '__main__':
+    import cv2
+    import extractpic
+
+    threading.Thread(target=capture_loop).start()
+
+    order = 0
+
+    testing = True
+    while testing:
+        try:
+            print "start %d" % order
+            order+=1
+            imgstr = get_framestream()
+            img = extractpic.string2image(imgstr,
+                                          (640, 480))
+            cv2.imwrite('a%d.jpg' % order, img)
+            time.sleep(1)
+        except:
+            testing = False
+            done = True
