22735244c9ed36d16ef6753e88218e9b04b4db74
xloc <tj-xzx@126.com>
2016-11-13 17:05:07 +0800

    Pack into methods

--- main_loop.py
+++ main_loop.py
@@ -16,84 +16,93 @@ IMAGE_WIDTH = 1000
 camera = cv2.VideoCapture(0)
 assert camera.isOpened(), 'Camera is not properly opened'
 
-# region PREPARE PICTURE FOR ANALYSIS
-# Wait
-time.sleep(1)
-# Take a picture
-valid, frame = camera.read()
-# Grayed
-gray = prop.grayed(frame)
-# Resize
-fitted = uf.resize2(gray, IMAGE_WIDTH)
-# endregion
-prepared = fitted
 
+def playground_analysis():
 
-# region PERSPECTIVE ANALYSIS
-pspt_param = detect_playground.perspective_analyse(prepared, threshold=THRESHOLD_VALUE)
-# endregion
-perspective_param = pspt_param
-
-
-# region Inverse perspective transform
-def warp_transform(image):
-    return prop.warp_perspective(image, pspt_param, PLAYGROUND_PIC_SIZE)
-# endregion
-warp_transform = warp_transform
-
-
-
-# region PLAYGROUND ANALYSIS
-
-# Inverse transform
-tailored = warp_transform(prepared)
-
-# Analysis playground
-playground, imthres = detect_playground.playground_analyse(tailored)
-# endregion
-playground = playground
-imthres = imthres
-
-
-imdebug = cv2.cvtColor(imthres, cv2.COLOR_GRAY2BGR)
-playground.draw(imdebug)
-imshow(imdebug)
-
-
-# WORK LOOP
-while True:
-    time.sleep(0.1)
-    # region PREPARE IMAGE FRAME
+    # region PREPARE PICTURE FOR ANALYSIS
+    # Wait
+    time.sleep(1)
+    # Take a picture
     valid, frame = camera.read()
-    # endregion
-    frame = frame
-
-    # region PRE-PROCESSING
     # Grayed
     gray = prop.grayed(frame)
     # Resize
     fitted = uf.resize2(gray, IMAGE_WIDTH)
-    # Warp transform
-    tailored = warp_transform(fitted)
-    # Threshold
-    imthres = prop.threshold_process(tailored, threshold=THRESHOLD_VALUE)
     # endregion
-    imout = imthres
+    prepared = fitted
 
-    # region CAR GESTURE ANALYSE
-    center, angle = None, None
-    try:
-        center, angle = detect_car.car_analysis(imout)
 
-    except AssertionError:
-        print "DETECT CAR ERROR"
+    # region PERSPECTIVE ANALYSIS
+    pspt_param = detect_playground.perspective_analyse(prepared, threshold=THRESHOLD_VALUE)
     # endregion
-    center, angle = center, angle
+    perspective_param = pspt_param
 
-    if center is None:
-        continue
 
-    imdebug = cv2.cvtColor(imout, cv2.COLOR_GRAY2BGR)
-    imdebug = detect_car.draw_car(imdebug, center, angle, (0, 255, 0))
-    cv2.imshow('show', imdebug)
-    print center
+    # region Inverse perspective transform
+    def warp_transform(image):
+        return prop.warp_perspective(image, pspt_param, PLAYGROUND_PIC_SIZE)
+    # endregion
+    warp_transform = warp_transform
+
+
+
+    # region PLAYGROUND ANALYSIS
+
+    # Inverse transform
+    tailored = warp_transform(prepared)
+
+    # Analysis playground
+    playground, imthres = detect_playground.playground_analyse(tailored)
+    # endregion
+    playground = playground
+    imthres = imthres
+
+
+    imdebug = cv2.cvtColor(imthres, cv2.COLOR_GRAY2BGR)
+    playground.draw(imdebug)
+    imshow(imdebug)
+
+    return warp_transform, playground
+
+def gesture_analysis(warp_transform):
+    # WORK LOOP
+    while True:
+        cv2.waitKey(100)
+        # region PREPARE IMAGE FRAME
+        valid, frame = camera.read()
+        # endregion
+        frame = frame
+
+        # region PRE-PROCESSING
+        # Grayed
+        gray = prop.grayed(frame)
+        # Resize
+        fitted = uf.resize2(gray, IMAGE_WIDTH)
+        # Warp transform
+        tailored = warp_transform(fitted)
+        # Threshold
+        imthres = prop.threshold_process(tailored, threshold=THRESHOLD_VALUE)
+        # endregion
+        imout = imthres
+
+        # region CAR GESTURE ANALYSE
+        center, angle = None, None
+        try:
+            center, angle = detect_car.car_analysis(imout)
+
+        except AssertionError:
+            print "DETECT CAR ERROR"
+        # endregion
+        center, angle = center, angle
+
+        if center is None:
+            continue
+
+        imdebug = cv2.cvtColor(imout, cv2.COLOR_GRAY2BGR)
+        imdebug = detect_car.draw_car(imdebug, center, angle, (0, 255, 0))
+        cv2.imshow('show', imdebug)
+        print center
+
+if __name__ == '__main__':
+    warp_fn, pg = playground_analysis()
+    gesture_analysis(warp_transform=warp_fn)
